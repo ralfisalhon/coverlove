ffcb43aabf475821bb6660af2d31c227c9bd7ff3
Ralfi Salhon <rifat.salhon@tufts.edu>
2019-03-29 13:31:59 +0800

    Random pic and random text added

--- App.js
+++ App.js
@@ -6,19 +6,86 @@ import {
   StatusBar,
   Dimensions,
   ImageBackground,
-  Image
+  Image,
+  Alert,
+  TouchableOpacity
 } from "react-native";
 
 const { width: windowWidth, height: windowHeight } = Dimensions.get("window");
 
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dimensions: 512,
+      imgURL: "https://source.unsplash.com/random/512x512",
+      colorIndex: 1,
+      colors: [
+        "#2ef7e6",
+        "#448af8",
+        "#56fa5a",
+        "#feda4b",
+        "#8745fb",
+        "#52fb9b",
+        "#fa714b",
+        "#f54b1e",
+        "#e0d0df",
+        "#54825c",
+        "#fed281",
+        "#050505",
+        "#d4d4d4",
+        "#ab55fa"
+      ]
+    };
+  }
+
+  randomPicture() {
+    this.setState(
+      {
+        dimensions: this.state.dimensions + 1
+      },
+      () =>
+        this.setState({
+          imgURL:
+            "https://source.unsplash.com/random/" +
+            this.state.dimensions +
+            "x" +
+            this.state.dimensions
+        })
+    );
+  }
+
+  randomColor() {
+    var colorIndex = Math.floor(Math.random() * this.state.colors.length);
+
+    if (colorIndex == this.state.colorIndex) {
+      this.randomColor();
+      return;
+    }
+
+    this.setState({ colorIndex });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
-        <View style={styles.albumView}>
-          <Image source={{ uri: "null" }} />
+        <View
+          style={{ backgroundColor: this.state.colors[this.state.colorIndex] }}
+        >
+          <ImageBackground
+            source={{ uri: this.state.imgURL }}
+            style={styles.imageView}
+          >
+            <Text style={styles.text}>Hello</Text>
+          </ImageBackground>
         </View>
+        <TouchableOpacity onPress={() => this.randomPicture()}>
+          <Text style={styles.text}>Random Picture</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => this.randomColor()}>
+          <Text style={styles.text}>Random Color</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -36,9 +103,9 @@ const styles = StyleSheet.create({
     fontFamily: "Avenir Next",
     fontSize: 24
   },
-  albumView: {
+  imageView: {
     height: windowWidth / 1.25,
     width: windowWidth / 1.25,
-    backgroundColor: "#8645fb"
+    opacity: 0.5
   }
 });
